25390d0a1151f09484381cfddd360553d5e51cfc
Tomasz Kuśmierczyk <tomaszkusmierczyk@letsweb.biz>
2014-01-22 13:37:00 +0100

    fixed problem with running from binary distibution

--- src/main/java/biz/letsweb/sqlite/SqliteSupportMain.java
+++ src/main/java/biz/letsweb/sqlite/SqliteSupportMain.java
@@ -1,9 +1,11 @@
 package biz.letsweb.sqlite;
 
+import biz.letsweb.sqlite.dao.ProjectDao;
 import biz.letsweb.sqlite.dao.ProjectDaoImpl;
 import biz.letsweb.sqlite.mvc.model.Project;
 import java.util.List;
 import java.util.Scanner;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,8 +18,8 @@ public class SqliteSupportMain {
         Scanner in = new Scanner(System.in);
         boolean run = true;
         System.out.println("type your command ...");
-        SqliteUtils.drop("activities_types", "activities", "types");
-        SqliteUtils.create();
+//        SqliteUtils.drop("activities_types", "activities", "types");
+//        SqliteUtils.create();
         while (run) {
             String command = in.nextLine();
             if (command.equalsIgnoreCase("projects")) {
@@ -25,6 +27,19 @@ public class SqliteSupportMain {
                 for (Project p : projects) {
                     System.out.println(p.getId() + ")" + p.getName());
                 }
+            }
+            if(command.startsWith("add project")){
+                final String[] splited = StringUtils.split(command);
+                ProjectDao projectDao = new ProjectDaoImpl();
+                Project newProject = new Project();
+                newProject.setName(splited[splited.length-1]);
+                projectDao.save(newProject);
+            }
+            if(command.startsWith("remove project")){
+                final String[] splited = StringUtils.split(command);
+                ProjectDao projectDao = new ProjectDaoImpl();
+                Project newProject = new Project();
+                newProject.setName(splited[splited.length-1]);
             }
             if (command.equals("stages")) {
                 System.out.println("stages: 4");
