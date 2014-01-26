3e292bdd9f422932624054fab8178a5fc9bf0806
Tomasz Kuśmierczyk <tomqsm@yahoo.co.uk>
2014-01-26 14:38:25 +0100

    associations between project/stage and stage/task added

--- src/main/resources/sql/timingdb/dev_create.sql
+++ src/main/resources/sql/timingdb/dev_create.sql
@@ -24,17 +24,29 @@ INSERT INTO types VALUES (null, 'project');
 INSERT INTO types VALUES (null, 'stage');
 INSERT INTO types VALUES (null, 'story');
 INSERT INTO types VALUES (null, 'task');
---adding projects
+
+-- adding projects
 INSERT INTO activities VALUES (null, 1, 'tomtom');
 INSERT INTO activities VALUES (null, 1, 'letsweb');
---adding stages
+
+-- adding stages
 INSERT INTO activities VALUES (null, 2, 'documentation');
 INSERT INTO activities VALUES (null, 2, 'implementation');
 INSERT INTO activities VALUES (null, 2, 'design');
---adding stories
-INSERT INTO activities VALUES (null, 3, 'story setting-up');
-INSERT INTO activities VALUES (null, 3, 'story fixing bug 123');
-INSERT INTO activities VALUES (null, 3, 'story fixing bug 456');
+SELECT a.id FROM activities a WHERE a.type_id = (SELECT id FROM types WHERE name='stage');
+
+-- adding stories
+INSERT INTO activities VALUES (null, 3, 'story setting-up'); --6
+INSERT INTO activities VALUES (null, 3, 'story fixing bug 123'); --7
+INSERT INTO activities VALUES (null, 3, 'story fixing bug 456'); --8
+SELECT a.id AS 'activity_id' FROM activities a WHERE a.type_id = (SELECT id FROM types WHERE name='story');
+
+-- adding tasks
+INSERT INTO activities VALUES (null, 4, 'task added try catch'); --9
+INSERT INTO activities VALUES (null, 4, 'task replaced dependency'); --10
+INSERT INTO activities VALUES (null, 4, 'task made a commit'); --11
+SELECT a.id AS 'activity_id' FROM activities a WHERE a.type_id = (SELECT id FROM types WHERE name='task');
+
 
 CREATE TABLE activities_associations (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -48,11 +60,26 @@ FOREIGN KEY(sub_activity_id) REFERENCES activities(id)
 INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='tomtom'), (SELECT id FROM activities WHERE name='documentation'));
 INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='tomtom'), (SELECT id FROM activities WHERE name='design'));
 INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='tomtom'), (SELECT id FROM activities WHERE name='implementation'));
-SELECT a.id FROM activities a WHERE a.type_id = (SELECT id FROM types WHERE name='stage');
+INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='letsweb'), (SELECT id FROM activities WHERE name='implementation'));
 
 -- associating stories with project
-INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='tomtom'), (SELECT id FROM activities WHERE name='documentation'));
-INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='tomtom'), (SELECT id FROM activities WHERE name='documentation'));
+INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='tomtom'), 6);
+INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='tomtom'), 7);
+INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='tomtom'), 8);
+INSERT INTO activities_associations VALUES (null, (SELECT id FROM activities WHERE name='letsweb'), 6);
+
+
+-- associating tasks with stories
+INSERT INTO activities_associations VALUES (null, 6, 9);
+INSERT INTO activities_associations VALUES (null, 6, 10);
+INSERT INTO activities_associations VALUES (null, 6, 11);
+INSERT INTO activities_associations VALUES (null, 7, 9);
 
 -- find stages associated with tomtom
-SELECT a.name FROM activities a JOIN activities_associations aa ON aa.sub_activity_id = a.id WHERE aa.sub_activity_id IN (SELECT a.id FROM activities a WHERE a.type_id = (SELECT id FROM types WHERE name='stage'));
+SELECT a.name FROM activities a JOIN activities_associations aa ON aa.sub_activity_id = a.id WHERE aa.sub_activity_id IN (SELECT ac.id FROM activities ac WHERE ac.type_id = (SELECT id FROM types WHERE name='stage')) AND aa.activity_id=(SELECT id FROM activities WHERE name='tomtom');
+
+-- find stories associated with tomtom
+SELECT a.name FROM activities a JOIN activities_associations aa ON aa.sub_activity_id = a.id WHERE aa.sub_activity_id IN (SELECT a.id FROM activities a WHERE a.type_id = (SELECT id FROM types WHERE name='story')) AND aa.activity_id=(SELECT id FROM activities WHERE name='tomtom');
+
+-- find tasks associated with story 'setting-up' (nr 6)
+SELECT a.name FROM activities a JOIN activities_associations aa ON aa.sub_activity_id = a.id WHERE aa.sub_activity_id IN (SELECT a.id FROM activities a WHERE a.type_id = (SELECT id FROM types WHERE name='task')) AND aa.activity_id=6;
